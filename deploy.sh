71fed7819e87cd9e69eb27841a3179c02ebb41a7
jarvisxiong <jarvisxiong@163.com>
2017-05-13 08:54:06 -0400

    “update”

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 hexo generate
-mdkir ../.deploy/jarvisxiong.github.io
-cp -R public/* ../.deploy/jarvisxiong.github.io
+mdkir -p ../deploy/jarvisxiong.github.io
+cp -R public/* ../deploy/jarvisxiong.github.io
 cd ../deploy/jarvisxiong.github.io
 git init 
 git remote add origin git@github.com:jarvisxiong/jarvisxiong.github.io.git
